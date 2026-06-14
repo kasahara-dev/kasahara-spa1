8a8e227a732081df6c0fc2a6975b3e783e7d6d61
kasahara <kasahara.maki.info@gmail.com>
2026-06-14 11:43:50 +0900

    case15

---
+++ laravel-next-app/tests/Feature/Case14ParentLogoutTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Faker\Factory;
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+
+class Case14ParentLogoutTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    protected function setUp():void{
+        parent::setUp();
+        $this->fake = Factory::create('ja_JP');
+        $this->accountId = (string) $this->fake->randomNumber(5);
+        $this->password = $this->fake->password();
+        $this->hashedPassword = Hash::make($this->password);
+        $this->name = $this->fake->name();
+        $this->user = User::create([
+            'account_id' => $this->accountId,
+            'role' => 'parent',
+            'name' => $this->name,
+            'password' => $this->hashedPassword,
+        ]);
+        $loginResponse = $this->postJson('/api/login', [
+            'login_id' => $this->accountId,
+            'password' => $this->password,
+            'role'     => 'parent',
+        ]);
+        $this->token = $loginResponse->json('token');
+    }
+    public function test_ログアウト(): void
+    {
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'tokenable_id'   => $this->user->id,
+            'tokenable_type' => get_class($this->user),
+        ]);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->postJson('/api/logout');
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'tokenable_id'   => $this->user->id,
+            'tokenable_type' => get_class($this->user),
+        ]);
+    }
+}
---
+++ laravel-next-app/tests/Feature/Case15ParentCalendarTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests\Feature;
+
+use Database\Seeders\CalendarSeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\EventSeeder;
+use Database\Seeders\AttendanceSeeder;
+use Tests\TestCase;
+use App\Models\Calendar;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Case15ParentCalendarTest extends TestCase
+{
+    use RefreshDatabase;
+    protected function setUp():void{
+        parent::setUp();
+        $this->seed([
+            CalendarSeeder::class,
+            UserSeeder::class,
+            EventSeeder::class,
+            AttendanceSeeder::class,
+        ]);
+        $this->user = User::where('role', 'parent')->first();
+        $this->accountId = $this->user->account_id;
+        $loginResponse = $this->postJson('/api/login', [
+            'login_id' => $this->accountId,
+            'password' => 'password',
+            'role'     => 'parent',
+        ]);
+        $this->token = $loginResponse->json('token');
+        $this->response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token,
+        ])->getJson('/api');
+    }
+    /**
+     * A basic feature test example.
+     */
+    public function test_カレンダーを全件取得できる(): void
+    {
+        $this->response->assertStatus(200);
+        $this->response->assertJsonStructure([
+            'config',
+            'calendar_data',
+        ]);
+        $calendarData = $this->response->json('calendar_data');
+        $this->assertCount(Calendar::count(), $calendarData);
+    }
+    public function test_イベントを全件取得できる()
+    {
+        $calendarData = $this->response->json('calendar_data');
+        foreach ($calendarData as $index => $jsonCalendar) {
+            $dbCalendar = Calendar::with('events')->find($jsonCalendar['id']);
+            $this->assertCount(
+                $dbCalendar->events->count(), 
+                $jsonCalendar['events'],
+                "Calendar ID: {$jsonCalendar['id']} の events 件数が一致しません。"
+            );
+        }
+    }
+    public function test_欠席を全件取得できる()
+    {
+        $calendarData = $this->response->json('calendar_data');
+        foreach ($calendarData as $index => $jsonCalendar) {
+            $dbCalendar = Calendar::with(['attendances' => function ($query) {
+                $query->where('user_id', $this->user->id)->where('status',1);
+            }])->find($jsonCalendar['id']);
+            $jsonAttendancesCount = collect($jsonCalendar['attendances'])
+                ->where('status', 1)
+                ->count();
+            $this->assertEquals(
+                $dbCalendar->attendances->count(), 
+                $jsonAttendancesCount,
+                "Calendar ID: {$jsonCalendar['id']} の欠席件数が一致しません。"
+            );
+        }
+    }
+    public function test_遅刻その他を全件取得できる()
+    {
+        $calendarData = $this->response->json('calendar_data');
+        foreach ($calendarData as $index => $jsonCalendar) {
+            $dbCalendar = Calendar::with(['attendances' => function ($query) {
+                $query->where('user_id', $this->user->id)->where('status',2);
+            }])->find($jsonCalendar['id']);
+            $jsonAttendancesCount = collect($jsonCalendar['attendances'])
+                ->where('status', 2)
+                ->count();
+            $this->assertEquals(
+                $dbCalendar->attendances->count(), 
+                $jsonAttendancesCount,
+                "Calendar ID: {$jsonCalendar['id']} の遅刻その他件数が一致しません。"
+            );
+        }
+    }
+}
